c51eb05d66914cdfab4b19717d6c36ec1d6ce50b
Nouman Malik <noumancodes@gmail.com>
2023-02-03 15:04:13 -0500

    Added discord gray colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,24 @@
-/** @type {import('tailwindcss').Config} */
+const colors = require('tailwindcss/colors')
+
 module.exports = {
-    mode: "jit",
-    content: ["./*.{html,js}"],
+    content: ['./src/**/*.{js,jsx}', './public/index.html'],
+    darkMode: 'class', // class, 'media' or boolean
     theme: {
         extend: {
             colors: {
-                primary: "#202225",
-                secondary: "#5865F2",
+                gray: {
+                    900: '#202225',
+                    800: '#2f3136',
+                    700: '#36393f',
+                    600: '#4f545c',
+                    400: '#d4d7dc',
+                    300: '#e3e5e8',
+                    200: '#ebedef',
+                    100: '#f2f3f5',
+                },
+            },
+            spacing: {
+                88: '22rem',
             },
         },
     },
